08fcab7da852c7a49e356f3a68b82582f0418c85
zhuluwuqing <>
2018-12-07 16:20:07 +0800

    actuator提供的13个接口

--- src/main/java/com/tgr/admin/configPoepertiesToBean/UseConfigBeanController.java
+++ src/main/java/com/tgr/admin/configPoepertiesToBean/UseConfigBeanController.java
@@ -1,8 +1,6 @@
 package com.tgr.admin.configPoepertiesToBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
